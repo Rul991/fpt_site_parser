8057dc0570e5ece43c5d939307cc88a43c8b764d
Rul991 <archerartem6@gmail.com>
2025-03-12 22:46:41 +0300

    updated for working on new site

--- parsing.py
+++ parsing.py
@@ -1,8 +1,17 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag, NavigableString
 
 import json
 import work_with_files as fw
+
+Element = Tag | NavigableString
+
+# ROOT = /html/body/section/div[0]/div[1]/div[0]/div
+# IMG = ROOT/div[0]/img
+# CONTENT = ROOT/div[1]/
+# LINK = CONTENT/a
+# SHORT_TEXT = CONTENT/p
+# DATE = CONTENT/div/time
 
 def parse_fpt_last_news():
     URL = 'https://feopoliteh.ru/news'
@@ -11,13 +20,20 @@ def parse_fpt_last_news():
     res = requests.get(URL)
 
     soup = BeautifulSoup(res.text, 'html.parser')
-    news_item = soup.find('main').find('section').find('div').find('div').find('div').find('div').find('div')
+    
+    root: Element = soup.find('section').find_all('div')[0].find_all('div')[1].find_all('div')[0].find('div')
+    img: Element = root.find('div').find('img')
+    content: Element = root.find_all('div', recursive=False)[1]
 
-    fpt_news['name'] = news_item.find('div').find('div', {'class': 'col-xs-7'}).find('div').find('div').find('a').text
-    fpt_news['href'] = 'https://feopoliteh.ru' + news_item.find('div').find('div', {'class': 'col-xs-7'}).find('div').find('div').find('a').get('href', '')
-    fpt_news['date'] = news_item.find('div').find('div', {'class': 'col-xs-7'}).find('div').find('div', {'class': 'news-date'}).text
-    fpt_news['shortText'] = news_item.find('div').find('div', {'class': 'col-xs-7'}).find('div').find('div', {'class': 'news-text'}).text.strip()
-    fpt_news['imgSrc'] = 'https://feopoliteh.ru' + news_item.find('div').find('div', {'class': 'col-xs-5'}).find('div').find('div').find('img').get('src', '')
+    link: Element = content.find('a')
+    short_text: Element = content.find('p')
+    date: Element = content.find('div').find('time')
+
+    fpt_news['name'] = link.text
+    fpt_news['href'] = link.get('href', '')
+    fpt_news['date'] = date.text
+    fpt_news['shortText'] = short_text.text
+    fpt_news['imgSrc'] = img.get('data-src', '')
 
     return fpt_news
 
@@ -26,3 +42,5 @@ def save_news(obj):
 
 def is_current_news_and_save_equal(current_news: dict):
     return json.load(open('json/saves.json', encoding='utf-8'))['name'] == current_news['name']
+
+print(parse_fpt_last_news())
